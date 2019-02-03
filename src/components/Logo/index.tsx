e117727a515de026d33f0535d2a21b1f03722ae6
Aaron Chen <chen_dong85@foxmail.com>
2019-02-03 20:49:54 +0800

    add the logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'react-native'
+
+interface props {
+    style: object,
+}
+
+
+const defaultStyle = {
+    width: 100,
+    height: 47,
+}
+
+const Logo: React.SFC<props> = ({ style = defaultStyle }: props) => <Image style={style} source={require('./logo.png')} />
+export default Logo
