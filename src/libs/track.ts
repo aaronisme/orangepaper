55c9508e09b1bcca4439a1bf2ad566496d45e887
Aaron Chen <chen_dong85@foxmail.com>
2019-02-07 00:07:54 +0800

    enable tracking function

---
+++ src/libs/track.ts
@@ -0,0 +1,20 @@
+import Amplitude from 'react-native-amplitude-analytics';
+import DeviceInfo from 'react-native-device-info';
+import { amplitudeApiKey } from '../config'
+
+let amplitude : Amplitude;
+
+if(!__DEV__) {
+    amplitude = new Amplitude(amplitudeApiKey);
+    const uuid = DeviceInfo.getUniqueID()
+    let userId = uuid || 'John Doe'
+    amplitude.setUserId(userId)
+}
+
+
+
+export const track = (event:string, options:object): void => {
+    if(!__DEV__) {
+        amplitude.logEvent(event, options)
+    }
+} 
